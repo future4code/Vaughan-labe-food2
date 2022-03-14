8fd499f31d8ec8802aca5c1dbf5925af7020c630
udimile <76756821+udimile@users.noreply.github.com>
2022-03-14 16:58:09 -0300

    configuração do tema

---
+++ src/constants/colors.js
@@ -0,0 +1,3 @@
+export const primaryColor = "#5CB646"
+export const secondaryColor = '#B9B9B9'
+export const neutralColor = '#000000'
---
+++ src/constants/theme.js
@@ -0,0 +1,35 @@
+import { createTheme } from '@mui/material/styles';
+import { primaryColor, neutralColor, secondaryColor } from './colors';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: primaryColor,
+
+    },
+    secondary:{
+        main: secondaryColor,
+    },
+    text: {
+        primary: neutralColor
+    }
+  },
+  typography: {
+    
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
+
+export default theme
