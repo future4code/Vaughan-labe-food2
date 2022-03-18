0d83c3666dc60b7082590b53dbd51222d58562ab
Adria Tavares <adria.tavares28@gmail.com>
2022-03-18 16:30:27 -0300

    Página de editar endereço

--- src/services/User.js
+++ src/services/User.js
@@ -18,13 +18,13 @@ export const login = (body, navigate) => {
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA3SHFaT0o3NnVrME9HUkI3NHhGIiwibmFtZSI6ImVzZm9tZWFkbyIsImVtYWlsIjoiZXNmb21lYWRvQGZ1dHVyZTQuY29tIiwiY3BmIjoiMTEwLjExMC4xMTAtMTAiLCJoYXNBZGRyZXNzIjp0cnVlLCJhZGRyZXNzIjoiUiBxdWFscXVlciBjb2lzYSwgMDAwLCA3MSAtIHF1YWxxdWVyIGx1Z2FyIiwiaWF0IjoxNjQ3MjkwMTA3fQ.l5VVhvVblsIvqn0eTuC0lKs_lqi6q3s5KEGMYkfASFo"
 
-export const updateProfile = (body) => {
+export const updateProfile = (body, navigate) => {
     const headers = { headers: { auth: token } }
     axios.put(`${BASE_URL}/profile`, body, headers)
     .then((res) => {
-        console.log(res.data)
+        alert("Perfil atualizado com sucesso!")
+        navigate(-1)
     }).catch((err) => {
-        // alert(err.response.data.message)
-        console.log(err)
+        alert(err.response.data.message)
     })
 }
