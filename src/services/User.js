02012ba8e85b608c95eabd22a5c32e931c2dbe39
Sofia Pitta Sesso <sofit_sops@hotmail.com>
2022-03-18 11:14:12 -0300

    Login Estilizado

--- src/services/User.js
+++ src/services/User.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 import { BASE_URL } from "../constants/URL";
+import { goToCreateAddress, goToHome } from '../routes/Coordinator'
 
-export const login = (body) => {
+export const login = (body, navigate) => {
+
 
     axios.post(`${BASE_URL}/login`, body)
         .then((res) => {
-            console.log(res.data)
+            if (res.data.user.hasAddress) {
+                goToHome(navigate)
+            } else {
+                goToCreateAddress(navigate)
+            }
+            localStorage.setItem('token', res.data.token)
         }).catch((err) => {
             alert(err.response.data.message)
         })
